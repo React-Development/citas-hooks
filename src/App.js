f77f1bec66da1316423066c6f238f7de4e3a5ddf
bgarcia95 <bgarcia.95@outlook.com>
2019-10-15 21:49:03 -0600

    Adding formulario into our project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
+
+function Formulario() {
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+
+      <form>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+        />
+
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño de la Mascota"
+        />
+
+        <label>Fecha</label>
+        <input type="date" className="u-full-width" name="fecha" />
+
+        <label>Hora</label>
+        <input type="time" className="u-full-width" name="hora" />
+
+        <label>Sintomas</label>
+        <textarea className="u-full-width" name="sintomas"></textarea>
+
+        <button type="submit" className="button-primary u-full-width">
+          Agregar
+        </button>
+      </form>
+    </Fragment>
+  );
+}
 
 function App() {
   // useState retorna 2 funciones
   // El primero es el state actual = this.state, el segundo es la funcion que actualiza el state = this.setState();
-  const[citas, guardarCita] = useState([]);
+  const [citas, guardarCita] = useState([]);
 
   console.log(citas);
 
   return (
-    <h1>Hola</h1>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column"></div>
+        </div>
+      </div>
+    </Fragment>
   );
-  
-
-
-  
-
-
 }
 
 export default App;
